b89a99ef0d19dd99d32ba2aa06c79320bd95e75c
Ramtin Tafaghodi <ramtin.tfgh2001@gmail.com>
2025-02-24 11:19:44 +0100

    bugs fixed

--- src/main/java/it/tafaq/springboot/onlineshop/RestController/BrandController.java
+++ src/main/java/it/tafaq/springboot/onlineshop/RestController/BrandController.java
@@ -2,8 +2,14 @@ package it.tafaq.springboot.onlineshop.RestController;
 
 import it.tafaq.springboot.onlineshop.dto.BrandDto;
 import it.tafaq.springboot.onlineshop.entity.Brand;
+import it.tafaq.springboot.onlineshop.entity.User;
 import it.tafaq.springboot.onlineshop.service.BrandService;
+import it.tafaq.springboot.onlineshop.service.UserService;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,16 +19,29 @@ import java.util.Optional;
 @RequestMapping("/api/brands")
 public class BrandController {
     private final BrandService brandService;
+    private final UserService userService;
 
-    public BrandController(BrandService brandService) {
+    public BrandController(BrandService brandService , UserService userService) {
         this.brandService = brandService;
+        this.userService = userService;
     }
 
     @PostMapping("/")
-    public void addNewBrand(@RequestBody BrandDto brandDto) {
+    public ResponseEntity<String> addNewBrand(@RequestBody BrandDto brandDto) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null ) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+        }
+        String email = authentication.getName();
+        User currentUser = userService.findByEmail(email);
+        if (currentUser.getRole().equals("ROLE_USER") || currentUser.getRole().equals("ROLE_ADMIN")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Users cannot add Brand");
+        }
         Brand newBrand = new Brand();
         newBrand.setName(brandDto.getName());
         brandService.save(newBrand);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Brand created");
     }
 
     @Cacheable(value = "brands" , key = "'all'")
